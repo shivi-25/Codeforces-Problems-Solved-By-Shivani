4dd7e84f512f24d0431cdb3939747a8712c1df86
shivi-25 <55458845+shivi-25@users.noreply.github.com>
2021-01-13 15:28:55 +0530

    Create 358A - Dima and Continuous Line.cpp

---
+++ 358A - Dima and Continuous Line.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     int n;
+     cin>>n;
+     int a[n];
+     for(int i=0;i<n;++i)
+     cin>>a[i];
+     if(n<=2)
+     {
+         cout<<"no";
+         return 0;
+     }
+     for(int i=0;i<n-1;++i)
+     for(int j=0;j<n-1;++j)
+     if((min(a[i], a[i+1]) < min(a[j], a[j+1]) && min(a[j], a[j+1]) < max(a[i], a[i+1]) && max(a[i], a[i+1]) < max(a[j], a[j+1])) ||
+			   (min(a[j], a[j+1]) < min(a[i], a[i+1]) && min(a[i], a[i+1]) < max(a[j], a[j+1]) && max(a[j], a[j+1]) < max(a[i], a[i+1])))
+			      {cout<<"yes";return 0;}
+     cout<<"no";
+     return 0;
+}
